4790268c0f7fc046d572272122d734e334095d9e
zhanibek <jonimoni84@gmail.com>
2025-09-15 23:23:49 +0500

    added images

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const form = document.getElementById("contactForm");
+
+if (form) {
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Спасибо за обращение! Мы свяжемся с вами в ближайшее время.");
+    form.reset();
+  });
+}
+// ====== Lightbox ======
+const galleryImages = document.querySelectorAll(".gallery img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const captionText = document.getElementById("caption");
+const closeBtn = document.querySelector(".close");
+
+galleryImages.forEach(img => {
+  img.addEventListener("click", () => {
+    lightbox.style.display = "block";
+    lightboxImg.src = img.src;
+    captionText.innerHTML = img.alt;
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
+
+lightbox.addEventListener("click", (e) => {
+  if (e.target === lightbox) {
+    lightbox.style.display = "none";
+  }
+});
--- js/script.ts
+++
@@ -1,9 +0,0 @@
-const form = document.getElementById("contactForm") as HTMLFormElement;
-
-if (form) {
-  form.addEventListener("submit", function(e) {
-    e.preventDefault();
-    alert("Спасибо за обращение! Мы свяжемся с вами в ближайшее время.");
-    form.reset();
-  });
-}
